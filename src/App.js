39bcf1ddc3d1ab28c5ae4d55b5d7875b8d8e9242
Glaucoed <gesverzuthe@gmail.com>
2022-08-30 18:22:43 -0300

    realizado requisito 2

--- src/App.js
+++ src/App.js
@@ -6,19 +6,53 @@ import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
-// import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
+  state = {
+    user: '',
+    isButtonDisable: true,
+  };
+
+  habilitaBotao = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { user } = this.state;
+      const numberMin = 2;
+      if (user.length > numberMin) {
+        this.setState({
+          isButtonDisable: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisable: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { user, isButtonDisable, loading } = this.state;
     return (
 
       <Switch>
-        <Route exact path="/" component={ Login } />
+        <Route
+          exact
+          path="/"
+          render={
+            (props) => (<Login
+              { ...props }
+              user={ user }
+              isButtonDisable={ isButtonDisable }
+              habilitaBotao={ this.habilitaBotao }
+              loading={ loading }
+            />)
+          }
+        />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
         <Route path="/profile" component={ Profile } />
-        {/* <Route path="/profile/edit" component={ ProfileEdit } /> */}
         <Route path="*" component={ NotFound } />
 
       </Switch>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  aguardandoFunction = (user) => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const { history } = this.props;
+      // console.log(history);
+      await createUser({ name: user });
+      history.push('/search');
+    });
+  };
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { user, habilitaBotao, isButtonDisable } = this.props;
+    const { loading } = this.state;
+    return (
+      loading ? (
+        <Loading />
+      )
+        : (
+          <div data-testid="page-login">
+            <h1>Login</h1>
+            <form>
+              <label htmlFor="login">
+                <input
+                  data-testid="login-name-input"
+                  name="user"
+                  value={ user }
+                  onChange={ habilitaBotao }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ isButtonDisable }
+                onClick={ () => this.aguardandoFunction(user) }
+              >
+                Entrar
+
+              </button>
+            </form>
+          </div>
+        )
+    );
   }
 }
-
+Login.propTypes = {
+  user: PropTypes.string,
+  habilitaBotao: PropTypes.func,
+  isButtonDisable: PropTypes.bool,
+}.isRequired;
 export default Login;
